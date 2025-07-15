46d03cc6af2a7f9db8116ea74df6f0a8d72e01e0
As-a-re <asare220044@gmail.com>
2025-07-15 10:07:23 +0000

    updates made

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 import { connect } from 'mongoose';
+
 const connectDB = async () => {
   try {
-    await connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
+    await connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.error(error.message);
+    console.error('MongoDB connection error:', error.message);
+    // Exit process with failure
     process.exit(1);
   }
 };
+
 export default connectDB;
